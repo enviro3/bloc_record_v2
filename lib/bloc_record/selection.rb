c4970342334c170c31faa40a1dfcc65b6a14b7bf
Caroline Vurlumis <cvurlumis@gmail.com>
2019-04-28 18:10:56 -0700

    Selectmethods_BlocRecord: complete

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,27 +1,83 @@
 require 'sqlite3'
 
 module Selection
+  def method_missing(method_name, *arguments)
+    method_name = method_name.to_s
+    if method_name.start_with?("find_by_")
+      column_they_hope_exists = method_name["find_by_".length..method_name.length]
+      if self.class.columns.any? {|column| column === column_they_hope_exists}
+        find_by(column_they_hope_exists, *arguments)
+      else
+        puts "Cannot do find_by on non-existant column #{column_they_hope_exists}"
+      end
+    else
+      puts "The method you tried to use, #{method_name} does not exist -- try another"
+    end
+
+  end
+
+  def find_in_batches(batch_size: 1000, start: 0, finish:)
+    if finish != nil && start > finish
+      return
+    end
+    limit = batch_size
+    current_position = start
+    while true
+      if finish != nil
+        distance_to_finish_line = [0, finish - current_position].max
+        limit = [batch_size, distance_to_finish_line].min
+      end
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{limit} OFFSET #{current_position};
+      SQL
+      rows = rows_to_array(rows)
+      yield rows
+      current_position = current_position + rows.length
+      if rows.length === 0
+        return
+      end
+    end
+  end
+
+  def find_each(batch_size:, start:, finish:)
+    find_in_batches(batch_size: batch_size, start: start, finish: finish) do |listings|
+      listings.each {|listing| yield listing}
+    end
+  end
+
+
+
   def find(*ids)
 
     if ids.length == 1
       find_one(ids.first)
-    else
+    elsif ids > 0
       rows = connection.execute <<-SQL
         SELECT #{columns.join ","} FROM #{table}
         WHERE id IN (#{ids.join(",")});
       SQL
 
-      rows_to_array(rows)
+      rows_to_array( rows)
+
+    else
+      puts 'The ID you entered is invalid.'
+      return -1
     end
   end
 
   def find_one(id)
-    row = connection.get_first_row <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE id = #{id};
-    SQL
+    if id.is_a?(Integer) && id > 0
+      row = connection.get_first_row <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id = #{id};
+      SQL
 
-    init_object_from_row(row)
+      init_object_from_row(row)
+    else
+      puts "ID is invalid"
+      return -1
+    end
   end
 
   def find_by(attribute, value)
