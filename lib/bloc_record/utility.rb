dd8c28f26ec7d59c8201048aecc439c0c599d5eb
Caroline Vurlumis <cvurlumis@gmail.com>
2019-05-19 19:51:17 -0700

    Update checkpoint: complete

--- lib/bloc_record/utility.rb
+++ lib/bloc_record/utility.rb
@@ -1,18 +1,18 @@
 module BlocRecord
   module Utility
-#   #1
+
     extend self
 
     def underscore(camel_cased_word)
-#      #2
+
        string = camel_cased_word.gsub(/::/, '/')
-#      #3
+
        string.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-#      #4
+
        string.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-#      #5
+
        string.tr!("-", "_")
-#      #6
+
        string.downcase
     end
 
